9a2e64ea5d733e811b4b93cebc35b3fa9d4e5a03
Junction <1429004361@qq.com>
2025-11-06 00:42:07 +0800

    构建用于用户认证的数据模型

--- alembic/env.py
+++ alembic/env.py
@@ -11,6 +11,7 @@ from alembic import context
 from src.core.config import settings
 from src.core.base_model import Base
 from src.dish.model import Dish
+from src.auth.model import User, AccessToken
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
